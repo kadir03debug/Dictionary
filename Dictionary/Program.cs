0e2182480dfa662181acd8ec1a11a457bb552e94
Ali Can <cucu.alican@gmail.com>
2021-01-23 17:53:19 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mydictionary<string, int> ogrenci = new mydictionary<string, int>();
+            ogrenci.Add(1, "kadir");
+            ogrenci.Add(2, "eda");
+            ogrenci.Add(3, "özge");
+            // 1. Eklenen yer
+            Console.WriteLine(ogrenci.Count);
+        }
+    }
+}
---
+++ Dictionary/mydictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class mydictionary<T, V>
+    {
+        V[] value;
+        T[] keys;
+        public mydictionary()
+        {
+            value = new V[0];
+            keys = new T[0];
+        }
+        // 2 Değiştirilen yerler
+        //V[] temp_value;
+        //T[] temp_keys;
+
+        public void Add(V val, T ke)
+        {
+            // 3 Değiştirilen yerler
+          V[]  temp_value = this.value;
+          T[]  temp_keys = keys;
+
+            keys = new T[keys.Length + 1];
+           this.value = new V[this.value.Length + 1];
+            //value yerine temp_keys yazılması gerek
+            for (int i = 0; i < temp_keys.Length; i++)
+            {
+                
+                    this.value[i] = temp_value[i];
+                    keys[i] = temp_keys[i];
+                
+            }
+
+            this.value[value.Length - 1] = val;
+            keys[keys.Length - 1] = ke;
+        }
+
+        //
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+    }
+}
